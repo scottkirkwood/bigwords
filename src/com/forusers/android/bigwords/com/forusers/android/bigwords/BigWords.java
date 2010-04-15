8e78b0b4b66c13cdd852c7e56064c5669dbfb8fd
scott <scott@forusers.com>
2010-04-14 21:18:11 -0300

    Initial commit of dummy app.

---
+++ src/com/forusers/android/bigwords/com/forusers/android/bigwords/BigWords.java
@@ -0,0 +1,15 @@
+package com.forusers.android.bigwords.com.forusers.android.bigwords;
+
+import com.forusers.android.bigwords.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BigWords extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
