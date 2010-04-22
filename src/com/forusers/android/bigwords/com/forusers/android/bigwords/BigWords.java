c49aa77d4998185d6cda3acfd806543fec38f17f
scott <scott@forusers.com>
2010-04-22 07:50:58 -0300

    No longer crashing.  Shows one axis on display.

--- src/com/forusers/android/bigwords/com/forusers/android/bigwords/BigWords.java
+++ src/com/forusers/android/bigwords/com/forusers/android/bigwords/BigWords.java
@@ -1,9 +1,15 @@
 package com.forusers.android.bigwords.com.forusers.android.bigwords;
 
-import com.forusers.android.bigwords.R;
-
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.forusers.android.bigwords.R;
 
 public class BigWords extends Activity {
     /** Called when the activity is first created. */
@@ -11,5 +17,39 @@ public class BigWords extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        SensorManager sm;
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+         
+        sm.registerListener(sl, sm.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+        		SensorManager.SENSOR_DELAY_NORMAL, new Handler());
+        textView = (TextView) findViewById(R.id.text);
     }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    }
+    
+    private final SensorEventListener sl = new SensorEventListener() {
+
+		@Override
+		public void onAccuracyChanged(Sensor arg0, int arg1) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			if (event.sensor.getType() != Sensor.TYPE_ORIENTATION) {
+				return;
+			}
+			float pitch = event.values[1];
+			float roll = event.values[2];
+			if (textView != null) {
+				textView.setText("Pitch:" + pitch);
+			}
+		}
+    };
+    
+    private TextView textView;
 }
