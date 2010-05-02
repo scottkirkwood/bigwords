3b3cc46c21a246a15cb8ea1af7a70484df7f5df5
scott <scott@forusers.com>
2010-05-02 12:38:06 -0300

    Start and stop, not working 100%

--- src/com/forusers/android/bigwords/BigWords.java
+++ src/com/forusers/android/bigwords/BigWords.java
@@ -1,41 +1,122 @@
 package com.forusers.android.bigwords;
 
-import java.awt.font.NumericShaper;
-import java.util.Formatter;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PowerManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.forusers.android.bigwords.R;
 import com.forusers.android.filter.AccelerometerFilter;
 
-public class BigWords extends Activity {
-    /** Called when the activity is first created. */
+public class BigWords extends Activity implements OnClickListener {
+    private static final int INVALID_ANGLE = 999;
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        SensorManager sm;
-        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-         
-        sm.registerListener(sl, sm.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-        		SensorManager.SENSOR_DELAY_NORMAL, new Handler());
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "DoNotDimScreen");
+
         textView = (TextView) findViewById(R.id.text);
+        
+        Button play = (Button) findViewById(R.id.play);
+        play.setOnClickListener(this);
     }
     
-    @Override
+	@Override
+	public void onClick(View v) {
+		if (R.id.play == v.getId()) {
+			onPlayOrPause();
+		}
+	}
+	
+	@Override
     public void onPause() {
-    	super.onPause();
+		super.onPause();
+		stopPlaying();
     }
+
+	@Override
+    public void onResume() {
+		super.onResume();
+    }
+
+    private void onPlayOrPause() {
+    	if (paused.get()) {
+    		startPlaying();
+    	} else {
+    		stopPlaying();
+    	}
+	}
     
-    private final SensorEventListener sl = new SensorEventListener() {
+	private void startPlaying() {
+		if (!paused.compareAndSet(true, false)) {
+			Log.i(TAG, "Attempted to start playing when already playing.");
+			return;
+		}
+		Button b = (Button) findViewById(R.id.play);
+		b.setText(R.string.stop);
+		
+		startTimer();
+		startListening();
+		wakeLock.acquire();
+	}
+
+	private void stopPlaying() {
+		if (paused.compareAndSet(false, true)) {
+			Log.i(TAG, "Attempted to stop playing when already stopped.");
+			return;
+		}
+		Button b = (Button) findViewById(R.id.play);
+		b.setText(R.string.play);
+		
+		stopTimer();
+		stopListening();
+		wakeLock.release();
+	}
+	
+	private void startTimer() {
+		timerHandler.removeCallbacks(nextWordTask);  // Just in case
+		timerHandler.postDelayed(nextWordTask, wpmInDelayMillis());
+	}
+
+	private long wpmInDelayMillis() {
+		return 60000 / wordsPerMinute;
+	}
+
+	private void stopTimer() {
+		timerHandler.removeCallbacks(nextWordTask);
+	}
+
+	private void startListening() {
+	    SensorManager sm;
+	    sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+	     
+	    sm.registerListener(orientationSensor, sm.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+	    		SensorManager.SENSOR_DELAY_NORMAL, new Handler());
+	    startAngle = INVALID_ANGLE;
+	}
+	
+	private void stopListening() {
+	    SensorManager sm;
+	    sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+	    sm.unregisterListener(orientationSensor);	    	
+	}
+	
+    private final SensorEventListener orientationSensor = new SensorEventListener() {
     	final AccelerometerFilter rollFilter = new AccelerometerFilter(50, 6);
     	
 		@Override
@@ -50,11 +131,28 @@ public class BigWords extends Activity {
 			}
 			float roll = event.values[2];
 			rollFilter.pushValue(roll);
-			if (textView != null) {
-				textView.setText("Roll: " + String.format("%.0f", rollFilter.getValue()));
+			float diff = rollFilter.getValue() - startAngle;
+		}
+    };
+ 
+    private final Runnable nextWordTask = new Runnable() {
+		@Override
+		public void run() {
+			if (textView == null) {
+				Log.e(TAG, "Missing textview in nextWordTask");
+				return;
 			}
+			textView.setText(String.format("Word%03d", wordIndex));			
+			timerHandler.postDelayed(this, wpmInDelayMillis());
 		}
     };
     
+    private static final String TAG = "BigWords";
     private TextView textView;
+    private PowerManager.WakeLock wakeLock;
+    private final Handler timerHandler = new Handler();
+    private AtomicBoolean paused = new AtomicBoolean(true);
+    private float startAngle = INVALID_ANGLE;
+    private int wordsPerMinute = 200;
+    private long wordIndex = 0;
 }
